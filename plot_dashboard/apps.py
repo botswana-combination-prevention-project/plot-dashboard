f068bcb5a7f53407d7aefa31b5c41d0a08fc5e2f
erikvw <ew2789@gmail.com>
2017-06-14 11:06:38 +0200

    initial

--- plot_dashboard/apps.py
+++ plot_dashboard/apps.py
@@ -5,3 +5,8 @@ from django.apps import AppConfig as DjangoAppConfig
 
 class AppConfig(DjangoAppConfig):
     name = 'plot_dashboard'
+    listboard_template_name = 'plot/listboard.html'
+    listboard_url_name = 'plot:listboard_url'
+    base_template_name = 'edc_base/base.html'
+    url_namespace = 'plot'  # FIXME: is this still neeed??
+    map_url_name = 'plot:map_url'
